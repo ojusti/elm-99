f38aa911480f6ee02f8b5f2482a8c239fbe36a48
Justinian Oprescu <ojusti@yahoo.fr>
2017-03-18 15:40:24 +0200

    Add Problem 7 - flatten

--- src/Src7.elm
+++ src/Src7.elm
@@ -7,4 +7,9 @@ type NestedList a
 
 flatten : NestedList a -> List a
 flatten nl =
-    []
+    case nl of
+      Elem x ->
+        [ x ]
+      SubList xs ->
+        List.map flatten xs 
+            |> List.concat
