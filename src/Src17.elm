a1798acf0e2aeaaa1b480ffe62bccb472f8363d5
Justinian Oprescu <ojusti@yahoo.fr>
2017-03-18 17:27:30 +0200

    Implement problem 17 - split

--- src/Src17.elm
+++ src/Src17.elm
@@ -3,5 +3,4 @@ module Src17 exposing (..)
 
 split : List a -> Int -> ( List a, List a )
 split list count =
-    -- your implementation here
-    ( [], [] )
+    ( List.take count list, List.drop count list )
