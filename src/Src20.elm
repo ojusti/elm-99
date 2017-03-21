e67397af2e6f62fff28c1f271cdd8cb8822caa0b
Justinian Oprescu <ojusti@yahoo.fr>
2017-03-21 21:48:34 +0200

    Add implement problem 20

--- src/Src20.elm
+++ src/Src20.elm
@@ -1,7 +1,16 @@
 module Src20 exposing (..)
 
+import List
+
 
 dropAt : Int -> List a -> List a
 dropAt n list =
-    -- your implementation here
-    []
+    (List.take (n-1) list) ++ (List.drop n list)
+    
+{- I like this one, but it is too much -
+    [ List.take (n - 1), List.drop n ]
+        |> List.map (\f -> f list)
+        |> List.concat
+--}
+
+
