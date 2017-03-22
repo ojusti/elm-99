8d27b6681eeb61f2852e556eebac4762c8c4c74a
Justinian Oprescu <ojusti@yahoo.fr>
2017-03-22 21:17:56 +0200

    Implement 19 - rotate

--- src/Src19.elm
+++ src/Src19.elm
@@ -1,6 +1,12 @@
 module Src19 exposing (..)
 
+import List
+
 
 rotate : Int -> List a -> List a
 rotate n list =
-    []
+    let
+        places =
+            n % List.length list
+    in
+        List.drop places list ++ List.take places list
